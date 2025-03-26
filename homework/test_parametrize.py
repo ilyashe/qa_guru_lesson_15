a406a02bc9cf24917d69ea31c305d739a193f22d
ilyashe <ilya.shebanoff@gmail.com>
2025-03-26 19:22:28 +0200

    Change test_parametrize.py

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,16 +2,26 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser, have
 
 
-@pytest.fixture()
-def browser():
-    pass
+@pytest.fixture(params=[(1920, 1080), (430, 932)])
+def setup_browser(request):
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+    yield
+    browser.quit()
 
 
-def test_github_desktop(browser):
-    pass
+@pytest.mark.parametrize("setup_browser", [(1920, 1080)], indirect=True, ids=["1920x1080"])
+def test_github_desktop(setup_browser):
+    browser.open("https://github.com/")
+    browser.all('button').element_by(have.text('Sign up')).click()
 
 
-def test_github_mobile(browser):
-    pass
+@pytest.mark.parametrize("setup_browser", [(430, 932)], indirect=True, ids=["430x932"])
+def test_github_mobile(setup_browser):
+    browser.open("https://github.com/")
+    browser.element('.Button-content').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
